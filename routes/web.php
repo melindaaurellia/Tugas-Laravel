21ba0c6b166f8b76f54bb7542aa34508af561abc
melindaaurellia <melindaaurelliah@gmail.com>
2020-10-19 07:51:04 +0700

    commit 4

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/coba', function () {
-    return view('coba');
-});
-
-Route::get('/coba/{no}', [CobaController::class, 'coba']);
-Route::get('/test', [CobaController::class, 'index']);
-Route::get('/test/{ke}', [CobaController::class, 'urutan']);
+Route::get('', [CobaController::class, 'index']);
+Route::get('/friends', [CobaController::class, 'index']);
+Route::get('/friends/create', [CobaController::class, 'create']);
+Route::post('/friends', [CobaController::class, 'store']);
